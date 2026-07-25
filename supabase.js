f3f0aa7cf6e4286d2e048831585d93600d4f369e
gentleacademicksa-web <gentle.academic.ksa@gmail.com>
2026-07-25 23:11:24 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://drannnhhxqiqmmkgywru.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_IqwjRusi7FZWq9bDdoYtPQ_QTlXCBku";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
